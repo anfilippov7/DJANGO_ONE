ca32fcf4c14ba47df9153909962ecd40bef8d913
anfilippov7 <106445819+anfilippov7@users.noreply.github.com>
2023-01-07 12:53:43 +0300

    DJANGO_7

--- 3.4-django-testing/django_testing/students/views.py
+++ 3.4-django-testing/django_testing/students/views.py
@@ -1,22 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-
 from students.filters import CourseFilter
 from students.models import Course
 from students.serializers import CourseSerializer
 
 
 class CoursesViewSet(ModelViewSet):
-
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     filter_backends = (DjangoFilterBackend, )
     filterset_class = CourseFilter
 
 
-# views.py
-# @api_view(['GET'])
-# def ping_view(request):
-#       return Response({"status": "Ok"})
